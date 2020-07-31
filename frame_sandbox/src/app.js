8c023dab036c458399867c4a88b1128028efc57b
Joe Lapp <arachnojoe@gmail.com>
2020-07-31 09:33:30 -0500

    Made a better frame_sandbox demo

--- frame_sandbox/src/app.js
+++ frame_sandbox/src/app.js
@@ -1,35 +1,20 @@
-var pixie;
-
 function initialize() {
   // canvas.setDimensions(width, height)
   // canvas.setDelay(delayMilliseconds)
 
-  const colors = {
-    " ": null,
-    "+": "blue",
-    "-": "green"
-  };
-  pixie = new Pixie(colors, [
-    " -",
-    "-+-",
-    " -"
-  ]);
-  canvas.setDelay(300);
+  canvas.setDelay(500);
 }
 
 function makeNextFrame(frameNumber) {
   // createFrame() - creates frame at size of the grid
-  // createFrame(width, height, initialValue=null)
+  // createFrame(width, height, initialColor=null)
   // reset() - re-initializes and restarts the animation
   // end() - stops the animation
 
   let f = canvas.createFrame();
   let s = frameNumber % 10;
-  pixie.draw(f, s, s);
-
-  // f[s][s] = "blue";
-  // f[1][s] = "green";
-  // f[8][9-s] = "red";
-
+  f[s][s] = "blue";
+  f[1][s] = "green";
+  f[8][9-s] = "red";
   return f;
 }
